fc1d587d6ec3a0718317e8ff2329807d3d3afeff
SabbirAhmed33 <sabbir146576@gmail.com>
2022-02-19 22:02:00 +0600

    almost on the finish line

--- JS/app.js
+++ JS/app.js
@@ -3,7 +3,7 @@ document.getElementById('calculate-button').addEventListener('click', function()
     // Get Income value
     const totalIncome = document.getElementById('income');
     const getInput = totalIncome.value;
-    
+
     // Input Error Handaler
     const positiveNumbers = document.getElementById('use-positive-numbers');
     const useOnlyNumbers = document.getElementById('use-numbers')
@@ -14,34 +14,72 @@ document.getElementById('calculate-button').addEventListener('click', function()
         useOnlyNumbers.style.display = 'block'; 
     }
    
+
     // Get Food Cost
     const foodCost = document.getElementById('food');
-    const foodCostValue = parseFloat(foodCost.value);
+    const foodCostValue = foodCost.value;
+
+    // Food Error Handaler
+    const positiveFoodNumbers = document.getElementById('usefood-positive-numbers');
+    const useOnlyFoodNumbers = document.getElementById('usefood-numbers')
+    if(foodCostValue<0){
+        positiveFoodNumbers.style.display = 'block';
+    }
+    else if(isNaN(foodCostValue)){
+        useOnlyFoodNumbers.style.display = 'block'; 
+    }
+
+
     // Get Rent Cost
     const rentCost = document.getElementById('rent');
-    const rentCostValue = parseFloat(rentCost.value);
+    const rentCostValue = rentCost.value;
+
+    // Rent Error Handaler
+    const positiveRentNumbers = document.getElementById('useRent-positive-numbers');
+    const useOnlyRentNumbers = document.getElementById('useRent-numbers')
+    if(rentCostValue<0){
+        positiveRentNumbers.style.display = 'block';
+    }
+    else if(isNaN(rentCostValue)){
+        useOnlyRentNumbers.style.display = 'block'; 
+    }
+
+
     // Get Clothes Cost
     const clothesCost = document.getElementById('clothes');
-    const clothesCostValue = parseFloat(clothesCost.value);
+    const clothesCostValue = clothesCost.value;
+
+    // Clothes Error Handaler
+    const positiveClothesNumbers = document.getElementById('useClothes-positive-numbers');
+    const useOnlyClothesNumbers = document.getElementById('useClothes-numbers')
+    if(clothesCostValue<0){
+        positiveClothesNumbers.style.display = 'block';
+    }
+    else if(isNaN(clothesCostValue)){
+        useOnlyClothesNumbers.style.display = 'block'; 
+    }
+
 
     // Get Sum of Total Cost
     const totalCost = document.getElementById('total-cost');
-    totalCost.innerText = parseFloat(foodCostValue + rentCostValue + clothesCostValue);
+    totalCost.innerText = parseFloat(foodCostValue) + parseFloat(rentCostValue) + parseFloat(clothesCostValue);
     const totalCostValue = totalCost.innerText;
+
 
     // Get Balance After Cost
     const balance = document.getElementById('balance');
-    balance.innerText = parseFloat(getInput - totalCostValue);
-    const balanceValue = balance.innerText;
+    balance.innerText = getInput - totalCostValue;
+    const balanceOver = balance.innerText;
+
+    // Balance Error Handaler
+    const overCost = document.getElementById('over-cost');
+    if(balanceOver<totalCostValue){
+        overCost.style.display = 'block';
+    }
+    
 })
 
- // Balance Error Handaler
-    /* const balanceOver = document.getElementById('balace');
-    const balanceOverValue = balanceOver.innerText;
-    const overCost = document.getElementById('over-cost');
-    if(balanceOverValue<0){
-        overCost.style.display = 'block';
-    } */
+
 
 // Get Savings And Remaining Balance
 document.getElementById('save-button').addEventListener('click', function(){
@@ -52,12 +90,21 @@ document.getElementById('save-button').addEventListener('click', function(){
     // Get Income value
     const totalIncome = document.getElementById('income');
     const getInput = totalIncome.value;
+
+
     // Get Savings
     const savingParsent = document.getElementById('savings-parsent');
     const savingParsentValue = savingParsent.value;
     const savingsAmount = document.getElementById('total-savings');
     const savingsAmountValue = parseFloat((savingParsentValue / 100)*getInput);
     savingsAmount.innerText = savingsAmountValue;
+    const totalSavings = savingsAmountValue;
+
+    // Savings Error Handaler
+    const noSavings = document.getElementById('no-savings');
+    if(totalSavings>balanceValue){
+        noSavings.style.display = 'block';
+    }
 
     // Get Remaining Balance
     const remainingBalance = document.getElementById('remaining-balance');
